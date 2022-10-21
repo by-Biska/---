32b7bb0e624a6254beecf977727afe1e40561733
Biska <varakinkirill3@mail.com>
2022-10-21 16:41:44 +0300

    2.4

---
+++ 2/2.3/1.py
@@ -0,0 +1,19 @@
+# j = ""
+def F(n):
+  j = str(n + 2*n + 1)
+  if n > 1:
+    j = str(3*n - 8)
+    F(n - 1)
+    F(n - 4)
+  
+# for i in range(100000):
+#   j = ""
+#   F(i)
+#   print(j)
+
+def f(n):
+  if n < 1:
+    return n + 2*n + 1
+  else:
+    return n + 2*n + 1 + 3*n - 8 + f(n-1) + f(n - 4)
+print(sum(str(f(28))))
---
+++ 2/2.3/2.py
@@ -0,0 +1,8 @@
+def f(n):
+  if n ==1:
+    return 1
+  if n > 1 and n % 2 == 0:
+    return n + f(n // 2)
+  if n > 1 and n % 2 == 1:
+    return f(n + 1)
+print(f(49))
