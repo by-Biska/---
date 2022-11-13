bdcaa4f3d4876e0e73fe9c9f9c7fa0037436e4a5
Biska <varakinkirill3@mail.com>
2022-11-13 19:48:57 +0300

    25_2023_homework

--- решение ЕГЭ/25/homework2023/10.py
+++ решение ЕГЭ/25/homework2023/10.py
@@ -1,6 +1,9 @@
 from fnmatch import fnmatch
 
 count = 0
-for x in range(699998, 99999999, 13):
-    if fnmatch(str(x), '*0??3') == False and fnmatch(str(x), '*4??2') == False and fnmatch(str(x), '*1*') == False:
-        print(x)
+for x in range(699998+13, 99999999, 13):
+    if fnmatch(str(x), '*0??3*') == False and fnmatch(str(x), '*4??2') == False and fnmatch(str(x), '*1*') == False:
+        print(x, sum(map(int, str(x))))
+        count += 1
+        if count == 5:
+            break
